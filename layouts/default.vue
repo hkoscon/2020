a6a8f6099e8eb9e5aa683654162aacffa1f74faf
Tony Yip <tony@terminal1.co>
2018-10-30 00:41:41 +0800

    Basic Structure

---
+++ layouts/default.vue
@@ -0,0 +1,66 @@
+<style scoped lang="scss">
+  .navbar-burger {
+    color: white;
+    &:hover {
+      color: white;
+    }
+  }
+</style>
+
+<template>
+  <div>
+    <nav
+      class="navbar"
+      role="navigation"
+      aria-label="main navigation">
+      <div class="navbar-brand">
+
+        <nuxt-link
+          class="navbar-item"
+          to="/2019/"
+        >HKOSCon 2019</nuxt-link>
+
+
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          @click="toggleNavbar"
+        >
+          <span aria-hidden="true"/>
+          <span aria-hidden="true"/>
+          <span aria-hidden="true"/>
+        </a>
+      </div>
+      <div
+        :class="{ 'is-active': showNavbar }"
+        class="navbar-menu"
+      >
+        <div class="navbar-start">
+          <a
+            class="navbar-item"
+            href="https://hkoscon.org/2018/timetable/"
+          >Timetable</a>
+        </div>
+      </div>
+    </nav>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      showNavbar: false,
+    };
+  },
+  methods: {
+    toggleNavbar() {
+      this.showNavbar = !this.showNavbar;
+    },
+  },
+};
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+const pkg = require('./package');
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'HKOSCon 2019',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'https://hkoscon.org/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Roboto|Material+Icons' },
+    ],
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#F3CB02' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/css/app.scss',
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    cssSourceMap: true,
+    publicPath: '/2019/',
+    loaders: {
+      scss: {
+        // eslint-disable-next-line
+        implementation: require('sass'),
+      },
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, { isDev, isClient }) {
+      config.devtool = '#source-map';
+      // Run ESLint on save
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        });
+      }
+    },
+  },
+};
