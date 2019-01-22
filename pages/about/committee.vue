40b960f28c89f5c7cc4bb39e5f4080bdcb78cbce
Tony Yip <tony@tonyhhyip.me>
2019-01-23 00:33:22 +0800

    Fix eslint issue

--- pages/about/committee.vue
+++ pages/about/committee.vue
@@ -13,11 +13,18 @@
     <section class="section">
       <div class="container content">
         <div>
-          <h3 class="title is-3">Web</h3>
+          <h3 class="title is-3">
+            Web
+          </h3>
           <ul class="aboutCommitteePage__member">
             <li>Koala Yeung</li>
             <li>
-              <a href="https://gitlab.com/404busters" target="_blank">Team 404 Busters</a>
+              <a
+                href="https://gitlab.com/404busters"
+                target="_blank"
+              >
+                Team 404 Busters
+              </a>
             </li>
           </ul>
         </div>
