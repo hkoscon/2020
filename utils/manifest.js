9f7709eafb93d30d92b6082965cdba7cbe9d59e9
Tony Yip <tony@tonyhhyip.me>
2019-02-20 21:03:41 +0800

    Fix scope in manifest

--- utils/manifest.js
+++ utils/manifest.js
@@ -2,5 +2,6 @@ const base = require('../manifest.json');
 
 base.start_url = process.env.PUBLIC_PATH;
 base.publicPath = process.env.PUBLIC_PATH;
+base.scope = process.env.PUBLIC_PATH;
 
 module.exports = base;
