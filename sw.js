4d8d09ddea3c94ecc0f09bf183e1b0c9aa1fd06b
Daisy <daisy.fung@gmail.com>
2020-05-21 10:25:03 +0000

    CI: Manually trigger rebuild from Event API Server
    * Rebuild (73e0de503041e7ed5742fb6510ce2727fc6836e9)

--- sw.js
+++ sw.js
@@ -303,7 +303,7 @@ self.__precacheManifest = [
   },
   {
     "url": "/2020//timetable/index.html",
-    "revision": "8970112c803fcab639259346b610642c"
+    "revision": "7ebc7324763ab7a2844ce739798a2937"
   },
   {
     "url": "/2020//topics/ansible-vm-kubernetes/index.html",
@@ -355,7 +355,7 @@ self.__precacheManifest = [
   },
   {
     "url": "/2020//topics/how-build-online-learning-platform-engages-and-educates-students-scale-amazon-kendra-amazon/index.html",
-    "revision": "fcac7d6ebac61e2704909b7448d60c5f"
+    "revision": "d6daeb80ed62bb5d215ee54c0d81da20"
   },
   {
     "url": "/2020//topics/how-i-do-troubleshooting-container-more-docker/index.html",
@@ -363,7 +363,7 @@ self.__precacheManifest = [
   },
   {
     "url": "/2020//topics/index.html",
-    "revision": "36e0386d6e471529977e586aefd82305"
+    "revision": "9ea56e1c0ee912ee64540db1eb17a47b"
   },
   {
     "url": "/2020//topics/julia-looks-python-feels-lisp-runs-cfortran/index.html",
@@ -451,7 +451,7 @@ self.__precacheManifest = [
   },
   {
     "url": "/2020//timetable/",
-    "revision": "8970112c803fcab639259346b610642c"
+    "revision": "7ebc7324763ab7a2844ce739798a2937"
   },
   {
     "url": "/2020//topics/ansible-vm-kubernetes/",
@@ -503,7 +503,7 @@ self.__precacheManifest = [
   },
   {
     "url": "/2020//topics/how-build-online-learning-platform-engages-and-educates-students-scale-amazon-kendra-amazon/",
-    "revision": "fcac7d6ebac61e2704909b7448d60c5f"
+    "revision": "d6daeb80ed62bb5d215ee54c0d81da20"
   },
   {
     "url": "/2020//topics/how-i-do-troubleshooting-container-more-docker/",
@@ -511,7 +511,7 @@ self.__precacheManifest = [
   },
   {
     "url": "/2020//topics/",
-    "revision": "36e0386d6e471529977e586aefd82305"
+    "revision": "9ea56e1c0ee912ee64540db1eb17a47b"
   },
   {
     "url": "/2020//topics/julia-looks-python-feels-lisp-runs-cfortran/",
